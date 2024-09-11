fc7c8488c34c01a87d8b43d67fe8af2339ca964c
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-11 10:52:41 -0700

    add layout to project layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,19 @@
 import { yekan } from "@/utils/fonts";
 
 import "./globals.css";
+import Layout from "@/layout/Layout";
 
 export const metadata = {
-  title: "آژانس مسکن",
+  title: "آژانس مسکن فلاحتی",
   description: "( آژانس مسکن فلاحتی ( خرید - فروش - رهن ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={yekan.className}>{children}</body>
+      <body className={yekan.className}>
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
