b1b0ab63cc6519cab134e3789358f297f368b51d
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-12 10:27:28 -0700

    create schema with mongoose for saving data in the mongodb

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+});
+
+const User = models.User || model("User", userSchema);
+
+export default User;
