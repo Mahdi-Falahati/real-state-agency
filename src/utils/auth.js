9ab211c30a95a78901a0caa07e055178d5f63650
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-12 11:40:36 -0700

    add a new function for validation email

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -11,4 +11,12 @@ async function verifyPassword(password, hashPassword) {
   return isValid;
 }
 
-export { hashPassword, verifyPassword };
+const validateEmail = (email) => {
+  const isValid =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  return isValid;
+};
+
+export { hashPassword, verifyPassword, validateEmail };
