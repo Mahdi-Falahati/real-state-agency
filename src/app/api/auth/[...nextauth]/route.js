976a686b17e85aa9cbbfb07f70d6fa2de1ef6ae0
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-13 10:23:26 -0700

    create sign in with next auth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import User from "@/models/User";
+import { verifyPassword, validateEmail } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("مشکلی در سرور رخ داده است");
+        }
+
+        if (!email || !password)
+          throw new Error("لطفا ایمیل و پسورد معتبر وارد کنید");
+
+        const emailValidation = validateEmail(email);
+        if (!emailValidation) throw new Error("لطفا ایمیل معتبر وارد کنید");
+
+        const user = await User.findOne({ email });
+        if (!user) throw new Error("لطفا ابتدا حساب کاربری ایجاد کنید");
+
+        const isValid = await verifyPassword(password, user.password);
+        if (!isValid) throw new Error("ایمیل یا رمزعبور اشتباه است");
+
+        return { email };
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
