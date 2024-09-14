405931d2615713efa45da0837cc7c2ba4da71a30
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-14 12:42:01 -0700

    handel state when user logged in show dashboard button if not logged in show login button

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,12 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { FiLogIn } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
 
-export default function Header() {
+export default async function Header() {
+  const session = await getServerSession(authOptions);
+
   return (
     <header className="flex p-4 mb-10 justify-between bg-blue-700 font-semibold rounded-b-md">
       <ul className="flex items-center justify-between min-w-[200px] text-white">
@@ -12,13 +17,22 @@ export default function Header() {
           <Link href="/buy-residential">آگهی ها</Link>
         </li>
       </ul>
-      <Link
-        href="/sign-in"
-        className="flex items-center hover:scale-105 justify-center bg-white text-blue-700 rounded-lg py-3 px-4"
-      >
-        <FiLogIn className="ml-2" />
-        <span className="tracking-widest">ورود</span>
-      </Link>
+      {session ? (
+        <Link
+          href="/dashboard"
+          className="flex items-center hover:scale-105 justify-center bg-white text-blue-700 rounded-lg py-3 px-4"
+        >
+          <FiUser />
+        </Link>
+      ) : (
+        <Link
+          href="/sign-in"
+          className="flex items-center hover:scale-105 justify-center bg-white text-blue-700 rounded-lg py-3 px-4"
+        >
+          <FiLogIn className="ml-2" />
+          <span className="tracking-widest">ورود</span>
+        </Link>
+      )}
     </header>
   );
 }
