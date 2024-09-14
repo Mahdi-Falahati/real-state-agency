f3bb32604c1f26cc3c607d12b4c9147c408b5b91
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-14 12:01:24 -0700

    create session provider and wrap it to app

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { yekan } from "@/utils/fonts";
 
 import "./globals.css";
 import Layout from "@/layout/Layout";
+import Providers from "@/providers/Providers";
 
 export const metadata = {
   title: "آژانس مسکن فلاحتی",
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={yekan.className}>
-        <Layout>{children}</Layout>
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
       </body>
     </html>
   );
