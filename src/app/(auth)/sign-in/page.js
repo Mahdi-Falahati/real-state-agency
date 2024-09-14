705389dbaf05056e7c33032b5aed64c0d1e6da10
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-14 12:34:06 -0700

    check user if logged in redirect to home route

--- src/app/(auth)/sign-in/page.js
+++ src/app/(auth)/sign-in/page.js
@@ -1,5 +1,11 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SignInPage from "@/template/SignInPage";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function SignIn() {
+export default async function SignIn() {
+  const session = await getServerSession(authOptions);
+  if (session) return redirect("/");
+
   return <SignInPage />;
 }
--- src/app/(auth)/sign-up/page.js
+++ src/app/(auth)/sign-up/page.js
@@ -1,5 +1,11 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SignUpPage from "@/template/SignUpPage";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function SignUp() {
+export default async function SignUp() {
+  const session = await getServerSession(authOptions);
+  if (session) return redirect("/");
+
   return <SignUpPage />;
 }
