79aa8f4a3f7c1b3d8e8298d388fabef5fd26d863
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-19 11:14:51 -0700

    add some style for responsive

--- src/components/layout/DashboardSideBar.jsx
+++ src/components/layout/DashboardSideBar.jsx
@@ -11,13 +11,13 @@ export default async function DashboardLayout({ children }) {
   const email = session?.user?.email;
 
   return (
-    <div>
-      <section className="flex flex-col justify-center items-center w-[250px] shadow-inner shadow-gray-400 p-3 rounded-md">
+    <div className="flex flex-col items-center md:items-start md:flex-row justify-around">
+      <section className="flex flex-col justify-center items-center w-[270px] md:w-[320px] h-[310px] shadow-inner shadow-gray-400 p-3 rounded-md">
         <HiOutlineUserCircle className="text-8xl text-gray-500" />
         <h2 className="font-semibold tracking-wide text-gray-600 shadow-inner p-2 shadow-gray-300 rounded-md">
           {email}
         </h2>
-        <div className="w-full h-[120px] text-gray-700 font-semibold flex flex-col justify-between items-start px-3 my-4">
+        <div className="w-full h-[130px] text-gray-700 font-semibold flex flex-col justify-between items-start px-3 my-4">
           <Link href="/dashboard" className={linkStyles}>
             <HiHashtag className="mr-2 text-2xl" />
             حساب کاربری
@@ -33,9 +33,9 @@ export default async function DashboardLayout({ children }) {
           <LogoutBTN />
         </div>
       </section>
-      {children}
+      <div className="p-2 m-3 w-full">{children}</div>
     </div>
   );
 }
 
-const linkStyles = "flex items-center hover:text-gray-900 hover:scale-105";
+const linkStyles = "my-1 flex items-center hover:text-gray-900 hover:scale-105";
