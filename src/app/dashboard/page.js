480031a476c38f4b6fab1a92bc88637b2e7cac75
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-19 09:56:10 -0700

    check user login or not for access

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,3 +1,14 @@
-export default function Dashboard() {
-  return <div></div>;
+import DashboardPage from "@/template/DashboardPage";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/sign-in");
+  }
+
+  return <DashboardPage />;
 }
