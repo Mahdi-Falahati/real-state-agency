5de12c92c2805c525f66da332c88ce2add168441
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-23 00:28:35 -0700

    remove unnecessary imports

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,8 @@
 import DashboardPage from "@/template/DashboardPage";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
 import connectDB from "@/utils/connectDB";
 
 export default async function Dashboard() {
   await connectDB();
-  
 
   return <DashboardPage />;
 }
