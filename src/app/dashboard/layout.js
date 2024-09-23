d49448e545e1e7f23b8b8d259ab1dfdb6ae0bf2e
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-23 00:27:05 -0700

    move validation user to layout page that all dashborad route can be validation

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,5 +1,14 @@
 import DashboardSideBar from "@/layout/DashboardSideBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function layout({ children }) {
+export default async function layout({ children }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/sign-in");
+  }
+
   return <DashboardSideBar>{children}</DashboardSideBar>;
 }
