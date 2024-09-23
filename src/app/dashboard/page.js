b4525ad7ba3f7c807702b5040494a45c0397ba9a
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-23 00:17:18 -0700

    add connect db

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,8 +2,10 @@ import DashboardPage from "@/template/DashboardPage";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import connectDB from "@/utils/connectDB";
 
 export default async function Dashboard() {
+  await connectDB();
   const session = await getServerSession(authOptions);
 
   if (!session) {
