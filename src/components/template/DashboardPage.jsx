ffc6bf7aafba8be6e11f9bfd6a4c080d24318a6f
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-23 00:18:35 -0700

    make dashboard page for account route

--- src/components/template/DashboardPage.jsx
+++ src/components/template/DashboardPage.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import User from "@/models/User";
+import { getServerSession } from "next-auth";
 
-export default function DashboardPage() {
-  return <div></div>;
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+  const user = await User.findOne({ email: session.user.email });
+  const date = new Date(user.createdAt).toLocaleDateString("fa-IR");
+
+  return (
+    <div>
+      <h3 className="text-3xl font-semibold my-2">سلام 👋</h3>
+      <p className="text-lime-700 font-semibold tracking-wider my-2 text-xl">
+        آگهی های خود را ثبت کنید تا هزاران نفر آن را مشاهده کنند .
+      </p>
+      <div className="flex items-center tracking-wider text-gray- italic font-semibold">
+        <p className="ml-2">تاریخ عضویت : </p>
+        <span>{date}</span>
+      </div>
+    </div>
+  );
 }
