87c414da0e8f6811e3475842f4ec5492c76654fb
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-25 07:31:15 -0700

    change style input

--- src/components/module/TextInput.jsx
+++ src/components/module/TextInput.jsx
@@ -36,9 +36,9 @@ export default function TextInput({
           name={name}
           value={profileData[name].value}
           onChange={ChangeValueHandler}
-          class="peer w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+          class="peer w-full bg-transparent placeholder:text-black text-black text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-black hover:border-black shadow-sm focus:shadow"
         />
-        <label class="absolute font-semibold cursor-text bg-white px-1 right-2.5 top-2.5 text-slate-400 text-sm transition-all transform origin-right peer-focus:-top-2 peer-focus:right-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
+        <label class="absolute font-semibold cursor-text bg-white px-1 right-2.5 top-2.5 text-black text-sm transition-all transform origin-right peer-focus:-top-2 peer-focus:right-2.5 peer-focus:text-xs peer-focus:text-black peer-focus:scale-90">
           {title}
         </label>
       </div>
