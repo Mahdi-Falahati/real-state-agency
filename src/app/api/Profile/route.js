a9878dbf83ea76b47521c998c3581cb45aa2aad4
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-27 07:03:18 -0700

    add connection to db and check user sign in or not

--- src/app/api/Profile/route.js
+++ src/app/api/Profile/route.js
@@ -1,1 +1,23 @@
-export async function POST(req) {}
+import connectDB from "@/utils/connectDB";
+
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا اول وارد حساب کاربری خود شود" },
+        { status: "401" }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
