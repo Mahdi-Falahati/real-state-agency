82ddba3e8ce201c29ef03528f3f2f6a0315fddd2
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-27 09:51:12 -0700

    query to the db and get all user's profiles

--- src/app/dashboard/my-profiles/page.js
+++ src/app/dashboard/my-profiles/page.js
@@ -1,5 +1,25 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Profile from "@/models/Profile";
+import User from "@/models/User";
 import MyProfilesPage from "@/template/MyProfilesPage";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
 
-export default function MyProfiles() {
-  return <MyProfilesPage />;
+export default async function MyProfiles() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+
+  const [user] = await User.aggregate([
+    { $match: { email: session.user.email } },
+    {
+      $lookup: {
+        from: "profiles",
+        foreignField: "userId",
+        localField: "_id",
+        as: "profiles",
+      },
+    },
+  ]);
+
+  return <MyProfilesPage profiles={user.profiles}/>;
 }
