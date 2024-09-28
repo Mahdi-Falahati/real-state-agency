39fecafe9157921088e1d4b2ffb6da855311cd70
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-28 06:26:30 -0700

    handle events delete and remove

--- src/components/module/DashboardCard.jsx
+++ src/components/module/DashboardCard.jsx
@@ -1,16 +1,27 @@
+"use client";
+
 import Card from "@/module/Card";
 import { LuClipboardEdit } from "react-icons/lu";
 import { RiDeleteBinLine } from "react-icons/ri";
 
 export default function DashboardCard({ data }) {
+  const deleteHandler = () => {};
+  const editHandler = () => {};
+
   return (
     <div className="my-5 shadow-inner rounded-md shadow-slate-400 pt-3">
       <div className="flex justify-around font-medium">
-        <button className={`${btnStyles} border-red-700 hover:bg-red-700 `}>
+        <button
+          onClick={deleteHandler}
+          className={`${btnStyles} border-red-700 hover:bg-red-700 `}
+        >
           <RiDeleteBinLine className={`${btnIconStyles} bg-red-700`} />
           <span className="px-2">حذف</span>
         </button>
-        <button className={`${btnStyles} border-green-500 hover:bg-green-500 `}>
+        <button
+          onClick={editHandler}
+          className={`${btnStyles} border-green-500 hover:bg-green-500 `}
+        >
           <LuClipboardEdit className={`${btnIconStyles} bg-green-500`} />
           <span className="px-2">ویرایش</span>
         </button>
