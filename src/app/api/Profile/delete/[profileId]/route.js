5b847683ef3bded7de563411519e75ae2ea19147
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-28 06:54:35 -0700

    make connection to db

--- src/app/api/Profile/delete/[profileId]/route.js
+++ src/app/api/Profile/delete/[profileId]/route.js
@@ -1,1 +1,12 @@
-export async function DELETE(req, context) {}
+import connectDB from "@/utils/connectDB";
+
+export async function DELETE(req, context) {
+  try {
+    await connectDB();
+  } catch (error) {
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
