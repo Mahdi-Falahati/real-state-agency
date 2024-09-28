d69a0146f1475580ea38a2b539725c88bcb10a67
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-28 06:55:48 -0700

    get params

--- src/app/api/Profile/delete/[profileId]/route.js
+++ src/app/api/Profile/delete/[profileId]/route.js
@@ -3,6 +3,8 @@ import connectDB from "@/utils/connectDB";
 export async function DELETE(req, context) {
   try {
     await connectDB();
+
+    const id = context.params.profileId;
   } catch (error) {
     return NextResponse.json(
       { error: "مشکلی در سرور رخ داده است" },
