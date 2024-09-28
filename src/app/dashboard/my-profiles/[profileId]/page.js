7506bdb8fbffcb91943c0dca3c84d1f690484f45
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-28 07:22:13 -0700

    handle error and send data to edit page

--- src/app/dashboard/my-profiles/[profileId]/page.js
+++ src/app/dashboard/my-profiles/[profileId]/page.js
@@ -1,7 +1,18 @@
+import Profile from "@/models/Profile";
+import AddProfilePage from "@/template/AddProfilePage";
 import connectDB from "@/utils/connectDB";
 
 export default async function EditPage({ params: { profileId } }) {
   await connectDB();
 
-  return <div>{profileId}</div>;
+  try {
+    const profile = await Profile.findOne({ _id: profileId });
+    return <AddProfilePage data={JSON.parse(JSON.stringify(profile))} />;
+  } catch (error) {
+    return (
+      <p className="text-red-600 text-center font-semibold tracking-widest">
+        این آگهی وجود ندارد...
+      </p>
+    );
+  }
 }
