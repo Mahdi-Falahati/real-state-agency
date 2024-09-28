834a8772b1c77d7edb48607e3dbad23589dd4270
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-28 08:02:57 -0700

    connect btn delete to api for deleting profile

--- src/components/module/DashboardCard.jsx
+++ src/components/module/DashboardCard.jsx
@@ -4,10 +4,27 @@ import Card from "@/module/Card";
 import { useRouter } from "next/navigation";
 import { LuClipboardEdit } from "react-icons/lu";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function DashboardCard({ data }) {
   const router = useRouter();
-  const deleteHandler = () => {};
+
+  const deleteHandler = async () => {
+    const req = await fetch(`/api/profile/delete/${data._id}`, {
+      method: "DELETE",
+    });
+
+    const res = await req.json();
+    if (res.error) {
+      toast.error(res.error);
+    } else {
+      toast.success(res.message);
+      setInterval(() => {
+        router.refresh();
+      }, 1500);
+    }
+  };
+
   const editHandler = () => {
     router.push(`/dashboard/my-profiles/${data._id}`);
   };
@@ -31,6 +48,7 @@ export default function DashboardCard({ data }) {
         </button>
       </div>
       <Card data={data} />
+      <ToastContainer position="top-center" limit={1} rtl={true} />
     </div>
   );
 }
