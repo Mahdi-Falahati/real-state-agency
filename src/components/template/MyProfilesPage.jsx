5273043d5b731541fc162b75f26051c377f322d6
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-28 03:39:37 -0700

    make my profiles page component and check if is not exist ay profile show a message

---
+++ src/components/template/MyProfilesPage.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { GiAquarium } from "react-icons/gi";
+import { BsArrowLeftShort } from "react-icons/bs";
+
+export default function MyProfilesPage({ profiles }) {
+  return (
+    <div>
+      {profiles.length ? null : (
+        <section className="flex items-center justify-center flex-col my-14">
+          <p className="flex items-center text-xl text-orange-700 font-semibold">
+            هیچ آگهی ثبت نشده است
+            <GiAquarium className="mr-2 text-2xl" />
+          </p>
+          <div className="flex items-center mt-1">
+            <p className="text-gray-600 ">میخوای یک آگهی ثبت کنی</p>
+            <Link
+              href="/dashboard/add-profile"
+              className="flex items-center mt-1 font-semibold underline text-gray-600"
+            >
+              <BsArrowLeftShort className="mx-1" />
+              ثبت آگهی
+            </Link>
+          </div>
+        </section>
+      )}
+    </div>
+  );
+}
