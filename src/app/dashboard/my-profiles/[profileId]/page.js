1c68667bb43e3cd19f0183424f0d89300b7124aa
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-09-28 07:15:23 -0700

    make connection to the db

--- src/app/dashboard/my-profiles/[profileId]/page.js
+++ src/app/dashboard/my-profiles/[profileId]/page.js
@@ -1,3 +1,7 @@
-export default function EditPage({ params: { profileId } }) {
+import connectDB from "@/utils/connectDB";
+
+export default async function EditPage({ params: { profileId } }) {
+  await connectDB();
+
   return <div>{profileId}</div>;
 }
