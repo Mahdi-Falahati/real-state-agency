bdce8a2a12068701eae585238dfd0e8ccad667e3
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-07 09:59:20 -0700

    add icon and cities name in seperate components

---
+++ src/constants/string.js
@@ -0,0 +1,19 @@
+const services = ["خرید", "فروش", "رهن", "اجاره"];
+const cities = [
+  "تهران",
+  "گیلان",
+  "همدان",
+  "کرج",
+  "مشهد",
+  "اصفهان",
+  "شیراز",
+  "کاشان",
+];
+const categories = {
+  apartment: "آپارتمان",
+  villa: "ویلا",
+  store: "مغازه",
+  office: "دفتر",
+};
+
+export { services, cities, categories };
