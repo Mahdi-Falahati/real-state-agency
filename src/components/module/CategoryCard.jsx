c33455f51f0bb1d3bcc74fc327c5d7111d1a7a45
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-07 11:32:01 -0700

    make category component for showing categories

---
+++ src/components/module/CategoryCard.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BiLink } from "react-icons/bi";
+
+export default function CategoryCard({ title, name }) {
+  return (
+    <Link href="/" className="shadow-lg pb-3 rounded-md hover:scale-105 m-5">
+      <Image
+        src={`/images/${name}.png`}
+        alt={title}
+        width={240}
+        height={144}
+        className="rounded-t-lg"
+      />
+      <p className="text-xl flex justify-center items-center mt-2 font-semibold tracking-wider text-indigo-900">
+        <BiLink className="ml-2" />
+        {title}
+      </p>
+    </Link>
+  );
+}
