7bcc8c3f4a27245bfbb856d854941f218c4f8ffc
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-07 11:04:36 -0700

    add animation and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,29 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      animation: {
+        "text-slide": "text-slide 25s cubic-bezier(0.83, 0, 0.17, 1) infinite",
+      },
+      keyframes: {
+        "text-slide": {
+          "0%": {
+            transform: "translateY(0%)",
+          },
+          "25%": {
+            transform: "translateY(-25%)",
+          },
+          "50%": {
+            transform: "translateY(-50%)",
+          },
+          "75%": {
+            transform: "translateY(-75%)",
+          },
+          "100%": {
+            transform: "translateY(-95%)",
+          },
+        },
+      },
     },
   },
   plugins: [],
