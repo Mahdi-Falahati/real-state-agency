7d05fd4bbfdc8d578e494c146d82422dca69f70e
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-08 11:57:53 -0700

    get all profile from db and pass to buy res... page

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,5 +1,26 @@
+import Profile from "@/models/Profile";
 import BuyResidentialPage from "@/template/BuyResidentialPage";
+import connectDB from "@/utils/connectDB";
 
 export default async function BuyResidential() {
-  return <BuyResidentialPage />;
+  try {
+    await connectDB();
+
+    const data = await Profile.find().select("-userId");
+
+    if (data.length === 0) {
+      return (
+        <p className="text-red-600 text-center font-semibold text-xl tracking-wider">
+          هیچ آگهی هنوز ثبت نشده است...
+        </p>
+      );
+    }
+    return <BuyResidentialPage data={data.data} />;
+  } catch (error) {
+    return (
+      <p className="text-red-600 text-center font-semibold text-xl tracking-wider">
+        در سرور مشکلی رخ داده است
+      </p>
+    );
+  }
 }
