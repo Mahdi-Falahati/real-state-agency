64dfc43fa8614c688fb003457824ac3eb7f5626e
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-10 06:19:18 -0700

    find profile from db

--- src/app/buy-residential/[profileId]/page.js
+++ src/app/buy-residential/[profileId]/page.js
@@ -1,5 +1,18 @@
+import Profile from "@/models/Profile";
 import ProfileDetailsPage from "@/template/ProfileDetailsPage";
+import connectDB from "@/utils/connectDB";
 
 export default async function ProfileDetails({ params: { profileId } }) {
-  return <ProfileDetailsPage />;
+  try {
+    await connectDB();
+    const profile = await Profile.findById(profileId);
+
+    return <ProfileDetailsPage />;
+  } catch (error) {
+    return (
+      <p className="text-red-600 text-center font-semibold text-xl tracking-wider">
+        در سرور مشکلی رخ داده است
+      </p>
+    );
+  }
 }
