8665102319c3bb8be312e28ffb8a5191ec3bfef2
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-11 10:10:53 -0700

    remove get data from db

--- src/app/dashboard/my-profiles/page.js
+++ src/app/dashboard/my-profiles/page.js
@@ -1,25 +1,5 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import Profile from "@/models/Profile";
-import User from "@/models/User";
 import MyProfilesPage from "@/template/MyProfilesPage";
-import connectDB from "@/utils/connectDB";
-import { getServerSession } from "next-auth";
 
 export default async function MyProfiles() {
-  await connectDB();
-  const session = await getServerSession(authOptions);
-
-  const [user] = await User.aggregate([
-    { $match: { email: session.user.email } },
-    {
-      $lookup: {
-        from: "profiles",
-        foreignField: "userId",
-        localField: "_id",
-        as: "profiles",
-      },
-    },
-  ]);
-
-  return <MyProfilesPage profiles={user.profiles}/>;
+  return <MyProfilesPage />;
 }
