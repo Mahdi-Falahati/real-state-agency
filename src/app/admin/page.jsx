5b5986f939b0ad112c69398180956df413832110
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-18 09:43:46 -0700

    check user role and redirect him

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,3 +1,18 @@
-export default function Admin() {
-  return <>Admin</>;
+import DashboardSideBar from "@/layout/DashboardSideBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import User from "@/models/User";
+
+export default async function Admin({ children }) {
+  const session = await getServerSession(authOptions);
+  if (!session) return redirect("/sign-in");
+  const user = await User.findOne({ email: session.user.email });
+  if (user.role != "ADMIN") return redirect("/dashboard");
+
+  return (
+    <DashboardSideBar email={user.email} role={user.role}>
+      {children}
+    </DashboardSideBar>
+  );
 }
