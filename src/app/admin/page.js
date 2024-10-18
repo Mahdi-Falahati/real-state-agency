cd78145b00f6bc96d77c42894e7e7c503169f5e3
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-18 10:43:50 -0700

    get all profiles with false published and pass to admin card as props

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -4,6 +4,7 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import User from "@/models/User";
 import AdminPage from "@/template/AdminPage";
+import Profile from "@/models/Profile";
 
 export default async function Admin({ children }) {
   const session = await getServerSession(authOptions);
@@ -11,9 +12,11 @@ export default async function Admin({ children }) {
   const user = await User.findOne({ email: session.user.email });
   if (user.role != "ADMIN") return redirect("/dashboard");
 
+  const profiles = await Profile.find({ published: false });
+
   return (
     <DashboardSideBar email={user.email} role={user.role}>
-      <AdminPage />
+      <AdminPage profiles={profiles} />
     </DashboardSideBar>
   );
 }
