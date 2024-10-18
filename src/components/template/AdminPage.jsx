2f9bea0c354de27dc7d1fcea6096e5438878ff39
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-18 12:47:25 -0700

    add flex to arrangement cardAdmin

--- src/components/template/AdminPage.jsx
+++ src/components/template/AdminPage.jsx
@@ -2,7 +2,7 @@ import CardAdmin from "@/module/CardAdmin";
 
 export default function AdminPage({ profiles }) {
   return (
-    <section>
+    <section className="flex justify-around items-center flex-wrap">
       {profiles?.map((profile) => (
         <CardAdmin key={profile._id} data={profile} />
       ))}
