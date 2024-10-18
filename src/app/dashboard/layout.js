b194b221a0b14797c235649d50fa4b25975c16ea
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-18 09:32:52 -0700

    find user and pass role and email as props to dashboardsidebar

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,6 +2,9 @@ import DashboardSideBar from "@/layout/DashboardSideBar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import User from "@/models/User";
+import { GiAquarium } from "react-icons/gi";
+import LogoutBTN from "@/module/LogoutBTN";
 
 export const metadata = {
   title: "پنل ادمین | سایت املاک فلاحتی",
@@ -14,5 +17,27 @@ export default async function layout({ children }) {
     return redirect("/sign-in");
   }
 
-  return <DashboardSideBar>{children}</DashboardSideBar>;
+  const user = await User.findOne({ email: session.user.email });
+  if (!user) {
+    return (
+      <>
+        <p className="flex items-center justify-center text-xl text-orange-700 font-semibold">
+          مشکلی پیش امده است
+          <GiAquarium className="mx-2 text-2xl" />
+          لطفا از حساب خود خارج شوید و دوباره تلاش کنید
+        </p>
+        <div className="flex justify-center">
+          <span className="w-[200px]">
+            <LogoutBTN />
+          </span>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <DashboardSideBar email={user.email} role={user.role}>
+      {children}
+    </DashboardSideBar>
+  );
 }
