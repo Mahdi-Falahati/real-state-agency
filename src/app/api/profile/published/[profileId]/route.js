170db2a0cbd301f8bf75ad361bb9bb14df71c20e
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-19 06:25:49 -0700

    check user info before published profile

--- src/app/api/profile/published/[profileId]/route.js
+++ src/app/api/profile/published/[profileId]/route.js
@@ -1,3 +1,4 @@
+import User from "@/models/User";
 import connectDB from "@/utils/connectDB";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
@@ -5,6 +6,42 @@ import { NextResponse } from "next/server";
 export async function PATCH(req) {
   try {
     await connectDB();
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "لطفا وارد حساب کاربری خود شوید",
+        },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "حساب کاربری یافت نشد",
+        },
+        { status: 404 }
+      );
+    }
+
+    if (user.role !== "ADMIN") {
+      return NextResponse.json(
+        {
+          error: "شما دسترسی لازم برای اینکار را ندارید",
+        },
+        { status: 200 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        error: "آگهی با موفقیت انتشار پیدا کرد",
+      },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json(
       { error: "مشکلی در سرور رخ داده است" },
