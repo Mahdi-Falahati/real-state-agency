1e5a9a3f9489ab80b24095fa3e5ebf1a8dc8512b
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-19 14:23:37 -0700

    seperate btns to components

--- src/components/module/CardAdmin.jsx
+++ src/components/module/CardAdmin.jsx
@@ -1,9 +1,12 @@
 import { icons } from "@/constants/icon";
 import { sp } from "@/utils/replaceNmber";
 import Link from "next/link";
+import { TiArrowBackOutline } from "react-icons/ti";
+import ConfirmBTN from "./ConfirmBTN";
+import RejectBtn from "./RejectBTN";
 
 export default function CardAdmin({ data }) {
-  const { category, title, price, location, phone } = data;
+  const { category, title, price, location, phone, _id } = data;
 
   return (
     <section className="my-7 mx-4 text-center text-gray-700 tracking-wider w-[280px] shadow-inner shadow-stone-400 py-4 px-2 rounded-md">
@@ -19,14 +22,17 @@ export default function CardAdmin({ data }) {
       <p className="font-medium my-2 leading-5 text-green-800">
         آدرس : {location}
       </p>
-      <div className="flex flex-col mx-2 font-semibold tracking-wider text-white">
-        <button className="bg-lime-600  rounded-lg py-2 mb-1 mt-3">
-          تایید آگهی
-        </button>
-        <button className="bg-red-600  rounded-lg py-2 my-1">
-          رد کردن اگهی
-        </button>
-        <Link href="/" className="bg-purple-700  rounded-lg py-2 my-1 ">
+      <div className="flex flex-col font-semibold tracking-wide">
+        <ConfirmBTN id={JSON.parse(JSON.stringify(_id))} />
+
+        <span className="h-[1px] w-full bg-gray-400"></span>
+        <RejectBtn />
+        <span className="h-[1px] w-full bg-gray-400"></span>
+        <Link
+          href={`/admin/${_id}`}
+          className="py-2 my-1 flex items-center hover:scale-110 justify-center"
+        >
+          <TiArrowBackOutline className="ml-1 animate-pulse text-purple-900 text-xl" />
           دیدن جزییات
         </Link>
       </div>
