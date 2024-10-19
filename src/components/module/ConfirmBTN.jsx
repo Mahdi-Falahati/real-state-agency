b4b0e996f13141707ec43ef7252b7d17e2271e87
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-19 09:17:58 -0700

    add spiner to btn

--- src/components/module/ConfirmBTN.jsx
+++ src/components/module/ConfirmBTN.jsx
@@ -1,21 +1,33 @@
 "use client";
+import { useState } from "react";
 import { GiConfirmed } from "react-icons/gi";
+import { BarLoader } from "react-spinners";
 
 export default function ConfirmBTN({ id }) {
+  const [loading, setLoading] = useState(false);
+
   const clickHandler = async () => {
+    setLoading(true);
     const req = await fetch(`/api/profile/published/${id}`, {
       method: "PATCH",
     });
     const res = await req.json();
+    setLoading(false);
   };
 
-  return (
-    <button
-      onClick={clickHandler}
-      className="text-green-700 flex items-center justify-center transition ease-in-out delay-150  hover:scale-110 py-2 mb-1 mt-3"
-    >
-      <GiConfirmed className="ml-3 animate-bounce text-xl" />
-      تایید آگهی
-    </button>
-  );
+  if (loading) {
+    return (
+      <BarLoader color="green" cssOverride={{ margin: "25px auto" }} loading />
+    );
+  } else {
+    return (
+      <button
+        onClick={clickHandler}
+        className="text-green-700 flex items-center justify-center transition ease-in-out delay-150  hover:scale-110 py-2 mb-1 mt-3"
+      >
+        <GiConfirmed className="ml-3 animate-bounce text-xl" />
+        تایید آگهی
+      </button>
+    );
+  }
 }
