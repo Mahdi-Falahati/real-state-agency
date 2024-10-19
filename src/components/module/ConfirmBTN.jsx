414a1d35276f82661905ef282cba0673c1175d1b
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-19 09:58:33 -0700

    add toast and reload page

--- src/components/module/ConfirmBTN.jsx
+++ src/components/module/ConfirmBTN.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { GiConfirmed } from "react-icons/gi";
 import { BarLoader } from "react-spinners";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function ConfirmBTN({ id }) {
   const [loading, setLoading] = useState(false);
@@ -13,6 +14,15 @@ export default function ConfirmBTN({ id }) {
     });
     const res = await req.json();
     setLoading(false);
+
+    if (res.error) {
+      toast.error(res.error);
+    } else {
+      toast.success(res.message);
+      setInterval(() => {
+        window.location.reload();
+      }, 1000);
+    }
   };
 
   if (loading) {
@@ -21,13 +31,16 @@ export default function ConfirmBTN({ id }) {
     );
   } else {
     return (
-      <button
-        onClick={clickHandler}
-        className="text-green-700 flex items-center justify-center transition ease-in-out delay-150  hover:scale-110 py-2 mb-1 mt-3"
-      >
-        <GiConfirmed className="ml-3 animate-bounce text-xl" />
-        تایید آگهی
-      </button>
+      <>
+        <button
+          onClick={clickHandler}
+          className="text-green-700 flex items-center justify-center transition ease-in-out delay-150  hover:scale-110 py-2 mb-1 mt-3"
+        >
+          <GiConfirmed className="ml-3 animate-bounce text-xl" />
+          تایید آگهی
+        </button>
+        <ToastContainer position="top-center" limit={1} rtl={true} />
+      </>
     );
   }
 }
