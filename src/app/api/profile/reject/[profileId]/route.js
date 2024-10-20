04ef0af3d1bfde04f4c89be1259639b533f9f265
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-20 00:07:07 -0700

    create connection with db

--- src/app/api/profile/reject/[profileId]/route.js
+++ src/app/api/profile/reject/[profileId]/route.js
@@ -1,1 +1,13 @@
-export async function PATCH(req, context) {}
+import connectDB from "@/utils/connectDB";
+import { NextResponse } from "next/server";
+
+export async function PATCH(req, context) {
+  try {
+    await connectDB();
+  } catch (error) {
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
