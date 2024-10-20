b656f3dc1a2386e2155069e6a15d4e178200e359
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-20 00:08:40 -0700

    check user logged in or not

--- src/app/api/profile/reject/[profileId]/route.js
+++ src/app/api/profile/reject/[profileId]/route.js
@@ -1,9 +1,20 @@
 import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function PATCH(req, context) {
   try {
     await connectDB();
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "لطفا وارد حساب کاربری خود شوید",
+        },
+        { status: 401 }
+      );
+    }
   } catch (error) {
     return NextResponse.json(
       { error: "مشکلی در سرور رخ داده است" },
