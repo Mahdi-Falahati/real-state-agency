73637b92985fe21c61f267c80e61436c71a04f69
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-20 07:19:40 -0700

    make aos init before use

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import Footer from "@/layout/Footer";
 import Header from "@/layout/Header";
 import "react-toastify/dist/ReactToastify.css";
+import { AOSInit } from "@/module/AOSInit";
 
 export default function Layout({ children }) {
   return (
     <>
+      <AOSInit />
       <Header />
       <main className="min-h-[318px]">{children}</main>
       <Footer />
